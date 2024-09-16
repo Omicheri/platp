e634dd9f6f88296852d12cd8f3fecfed6b425772
Omer <o.akay@xefi.fr>
2024-09-16 18:55:02 +0200

    ajout de group middleware dans mes routes, changement des factory plat pour lier a l'utilisateur

--- database/migrations/2024_09_16_083012_create_role_table.php
+++ database/migrations/2024_09_16_083012_create_role_table.php
@@ -7,28 +7,21 @@ use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        $role = Role::create(['name' => 'administrator']);
 
+        $permission_create = Permission::create(['name' => 'create plats']);
+        $permission_edit = Permission::create(['name' => 'edit plats']);
+        $permission_destroy = Permission::create(['name' => 'destroy plats']);
 
-            $role = Role::create(['name' => 'administrator']);
-
-            $permission_create = Permission::create(['name' => 'create plats']);
-            $permission_edit = Permission::create(['name' => 'edit plats']);
-            $permission_destroy = Permission::create(['name' => 'destroy plats']);
-
-            $role->givePermissionTo($permission_create);
-            $role->givePermissionTo($permission_edit);
-            $role->givePermissionTo($permission_destroy);
-
-
-
-
+        $role->givePermissionTo($permission_create);
+        $role->givePermissionTo($permission_edit);
+        $role->givePermissionTo($permission_destroy);
 
     }
 
